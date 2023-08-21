2a717e8c983b92cc1128e9dcb4de9c637f94ead5
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2023-08-21 22:08:14 +0100

    update

--- cypress/integration/my-test/my-test.test.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("can navigate pages", () => {
-  it("can navigate pages", () => {
-    cy.visit("https://example.cypress.io/");
-    cy.contains("within").click();
-    cy.url().should("include", "/commands/querying");
-  });
-});
